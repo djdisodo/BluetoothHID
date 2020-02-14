32128deb1d1f191c4b664e284b6db2804a348593
djdisodo <djdisodo@gmail.com>
2020-02-14 13:38:01 +0900

    add Rx, Ry to gamepad (need test)

--- src/main/java/com/github/bluetoothhid/input/GamePad.java
+++ src/main/java/com/github/bluetoothhid/input/GamePad.java
@@ -8,41 +8,42 @@ public class GamePad implements HidDescription {
 	@Override
 	public byte[] getDescription(byte id) {
 		return new byte[] {
-				0x05, 0x01,        // USAGE_PAGE (Generic Desktop)
-				0x09, 0x05,        // USAGE (Game Pad)
-				(byte) 0xa1, 0x01, // COLLECTION (Application)
-				(byte) 0x85, id, //    Report ID
-				(byte) 0xa1, 0x00, //   COLLECTION (Physical)
-				0x05, 0x09,        //     USAGE_PAGE (Button)
-				0x19, 0x01,        //     USAGE_MINIMUM (Button 1)
-				0x29, 0x04,        //     USAGE_MAXIMUM (Button 4)
-				0x15, 0x00,        //     LOGICAL_MINIMUM (0)
-				0x25, 0x01,        //     LOGICAL_MAXIMUM (1)
-				0x75, 0x01,        //     REPORT_SIZE (1)
-				(byte) 0x95, 0x04, //     REPORT_COUNT (4)
-				(byte) 0x81, 0x02, //     INPUT (Data,Var,Abs)
-				0x75, 0x04,        //     REPORT_SIZE (4)
-				(byte) 0x95, 0x01, //     REPORT_COUNT (1)
-				(byte) 0x81, 0x03, //     INPUT (Cnst,Var,Abs)
-				0x05, 0x01,        //     USAGE_PAGE (Generic Desktop)
-				0x09, 0x30,        //     USAGE (X)
-				0x09, 0x31,        //     USAGE (Y)
-				0x15, (byte) 0x81, //     LOGICAL_MINIMUM (-127)
-				0x25, 0x7f,        //     LOGICAL_MAXIMUM (127)
-				0x75, 0x08,        //     REPORT_SIZE (8)
-				(byte) 0x95, 0x02, //     REPORT_COUNT (2)
-				(byte) 0x81, 0x02, //     INPUT (Data,Var,Abs)
-				(byte) 0xc0,       //   END_COLLECTION
-				(byte) 0xc0        // END_COLLECTION
+				(byte)0x05, (byte)0x01,                    // USAGE_PAGE (Generic Desktop)
+				(byte)0x09, (byte)0x05,                    // USAGE (Game Pad)
+				(byte)0xa1, (byte)0x01,                    // COLLECTION (Application)
+				(byte)0xa1, (byte)0x00,                    //   COLLECTION (Physical)
+				(byte)0x85, id,                	   //     REPORT_ID (3)
+				(byte)0x05, (byte)0x09,                    //     USAGE_PAGE (Button)
+				(byte)0x19, (byte)0x01,                    //     USAGE_MINIMUM (Button 1)
+				(byte)0x29, (byte)0x10,                    //     USAGE_MAXIMUM (Button 16)
+				(byte)0x15, (byte)0x00,                    //     LOGICAL_MINIMUM (0)
+				(byte)0x25, (byte)0x01,                    //     LOGICAL_MAXIMUM (1)
+				(byte)0x75, (byte)0x01,                    //     REPORT_SIZE (1)
+				(byte)0x95, (byte)0x10,                    //     REPORT_COUNT (16)
+				(byte)0x81, (byte)0x02,                    //     INPUT (Data,Var,Abs)
+				(byte)0x05, (byte)0x01,                    //     USAGE_PAGE (Generic Desktop)
+				(byte)0x09, (byte)0x30,                    //     USAGE (X)
+				(byte)0x09, (byte)0x31,                    //     USAGE (Y)
+				(byte)0x09, (byte)0x33,                    //     USAGE (Rx)
+				(byte)0x09, (byte)0x34,                    //     USAGE (Ry)
+				(byte)0x15, (byte)0x81,                    //     LOGICAL_MINIMUM (-127)
+				(byte)0x25, (byte)0x7f,                    //     LOGICAL_MAXIMUM (127)
+				(byte)0x75, (byte)0x08,                    //     REPORT_SIZE (8)
+				(byte)0x95, (byte)0x04,                    //     REPORT_COUNT (4)
+				(byte)0x81, (byte)0x02,                    //     INPUT (Data,Var,Abs)
+				(byte)0xc0,                          //     END_COLLECTION
+				(byte)0xc0
 		};
 	}
 
 	final public static class Report implements com.github.bluetoothhid.input.Report {
-		final private ByteBuffer report = ByteBuffer.allocate(3);
-		public Report(byte channel, @Button byte button, byte x, byte y) {
+		final private ByteBuffer report = ByteBuffer.allocate(5);
+		public Report(byte channel, @Button byte button, byte x, byte y, byte Rx, byte Ry) {
 			report.put((byte)((channel << 4) | button));
 			report.put(x);
 			report.put(y);
+			report.put(Rx);
+			report.put(Ry);
 		}
 
 		public byte getChannel() {
@@ -83,6 +84,22 @@ public class GamePad implements HidDescription {
 
 		public void setY(byte y) {
 			report.put(2, y);
+		}
+
+		public byte getRx() {
+			return report.get(3);
+		}
+
+		public void setRx(byte x) {
+			report.put(3, x);
+		}
+
+		public byte getRy() {
+			return report.get(4);
+		}
+
+		public void setRy(byte y) {
+			report.put(4, y);
 		}
 		@Override
 		public byte[] build() {
